48e94dafa9f83f1ae828414ac069523a6099ed6e
Venkat Rangala <venkatrangala@gmail.com>
2020-08-04 12:01:04 +0100

    Api changes

--- MMU.FileUpload.Api/Startup.cs
+++ MMU.FileUpload.Api/Startup.cs
@@ -25,14 +25,13 @@ namespace WebApi
     public class Startup
     {
         private readonly IConfiguration _configuration;
+        //public IConfiguration Configuration { get; }
         public Startup(IConfiguration configuration)
         {
             _configuration = configuration;
             //Configuration = configuration;
         }
-
-        //public IConfiguration Configuration { get; }
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
