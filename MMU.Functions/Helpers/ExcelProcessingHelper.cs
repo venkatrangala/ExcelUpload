b15347db477bcbdc0281e3bca628112018e8f1dd
Venkat Rangala <46047810@ad.mmu.ac.uk>
2020-08-14 13:09:34 +0100

    BusinessNames changes

--- MMU.Functions/Helpers/ExcelProcessingHelper.cs
+++ MMU.Functions/Helpers/ExcelProcessingHelper.cs
@@ -9,6 +9,7 @@ using NPOI.SS.UserModel;
 using NPOI.SS.Util;
 using NPOI.XSSF.UserModel;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -50,7 +51,7 @@ namespace MMU.Functions.Helpers
         //[AllowAnonymous]
         public async Task<IActionResult> ReadFilesFromBlob(string blobName)
         {
-           var containerName = "excel";
+            var containerName = "excel";
             //string blobName = "rv.xlsx";
             var azureStorageBlobOptions = new AzureStorageBlobOptions(_configuration);
 
@@ -127,6 +128,28 @@ namespace MMU.Functions.Helpers
             //Select BusinessMeaningName,id Courselevelid from ACCourselevel
             //Select BusinessMeaningName,id EnrollmentmodeId from ACEnrollmentMode
 
+            var priceGroupQuery = @"Select Id, BusinessMeaningName from BCPriceGroup";
+            var courseLevelQuery = @"Select Id, BusinessMeaningName from ACCourselevel";
+            var enrollmentModelQuery = @"Select Id, BusinessMeaningName from ACEnrollmentMode";
+
+            var priceGroupList = new List<BusinessNames>();
+            var courseLevelList = new List<BusinessNames>();
+            var enrollmentModelList = new List<BusinessNames>();
+
+            try
+            {
+                //recordId = _dataService.Query<int>("u4clone", query).FirstOrDefault();
+                priceGroupList = _dataService.Query<BusinessNames>("u4clone", priceGroupQuery).ToList();
+                courseLevelList = _dataService.Query<BusinessNames>("u4clone", courseLevelQuery).ToList();
+                enrollmentModelList = _dataService.Query<BusinessNames>("u4clone", enrollmentModelQuery).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw;
+                //We dont need to handle as we just update the record with no ID
+            }
+
+
             //CourseId
             //CourseTitle
             //StartDate
@@ -143,10 +166,19 @@ namespace MMU.Functions.Helpers
             using FileStream rstr = new FileStream(fileName, FileMode.Open, FileAccess.Read);
             IWorkbook workbook = new XSSFWorkbook(rstr);
             var sheet = workbook.GetSheet(sheetName);
-            IRow headerRow = sheet.GetRow(1); //Send Row is Header 
+            IRow headerRow = sheet.GetRow(1); //Header 
             int cellCount = headerRow.LastCellNum;
 
             string courseIdColumn = "B";
+            string MinEnrolledColumn = "D";
+            string MaxEnrolledColumn = "E";
+            string PriceGroupIdColumn = "F";
+            string CourseLevelIdColumn = "G";
+            string EnrollmentModeIdColumn = "H";
+            string RecordIdColumn = "I";
+            string ResultColumn = "J";
+            string ErrorColumn = "K";
+
             for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Values from 2nd row
             {
                 IRow row = sheet.GetRow(i);
@@ -154,55 +186,34 @@ namespace MMU.Functions.Helpers
                 if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
 
                 string courseId = string.Empty;
-                string courseTitle = string.Empty;
-                string startDate = string.Empty;
-                string endDate = string.Empty;
+                //string courseTitle = string.Empty;
+                //string startDate = string.Empty;
+                //string endDate = string.Empty;
                 string successValue = string.Empty;
                 string invalidDates = string.Empty;
                 dynamic errorValue = null;
                 int recordId = 0;
-                int id;
+                //int id;
+
+                var payload = new CourseOfferingTemplate();
                 HttpResponseMessage httpResponseMessage;
-                var updateResult = 0;
+                //var updateResult = 0;
+
                 int j = 1; //Leave the first column blank
                 //Columns from each row
                 for (j = row.FirstCellNum; j < cellCount; j++)
                 {
                     if (row.GetCell(j) != null)
                     {
-                        var temp1 = new CellReference(row.GetCell(j));
-                        var reference = temp1.FormatAsString();
+                        //var temp1 = new CellReference(row.GetCell(j));
+                        //var reference = temp1.FormatAsString();
+                        var reference = new CellReference(row.GetCell(j)).FormatAsString();
                         ICell cell;
 
-                        //Get the CourseId & AcademicPeriod & fetch RecordID
+                        //Get the CourseId & fetch RecordID
                         if (reference.StartsWith(courseIdColumn)) //CourseId
                         {
-                            courseId = row.GetCell(j).StringCellValue;
-                        }
-                        
-
-
-
-
-                        if (reference.StartsWith(MinEnrolledColumn)) //MinEnrolled
-                        {
-                            courseId = row.GetCell(j).StringCellValue;
-                        }
-                        if (reference.StartsWith(MaxEnrolledColumn)) //MaxEnrolled
-                        {
-                            courseId = row.GetCell(j).StringCellValue;
-                        }
-                        if (reference.StartsWith(PriceGroupIdColumn)) //PriceGroupIdColumn
-                        {
-                            courseId = row.GetCell(j).StringCellValue;
-                        }
-                        if (reference.StartsWith(CourseLevelIdColumn)) //CourseLevelIdColumn
-                        {
-                            courseId = row.GetCell(j).StringCellValue;
-                        }
-                        if (reference.StartsWith(EnrollmentModeIdColumn)) //EnrollmentModeIdColumn
-                        {
-                            courseId = row.GetCell(j).StringCellValue;
+                            courseId = row.GetCell(j).StringCellValue.Trim();
                         }
 
                         //if (reference.StartsWith("C")) //Course Title
@@ -210,7 +221,7 @@ namespace MMU.Functions.Helpers
                         //    courseTitle = row.GetCell(j).StringCellValue;
                         //}
 
-                        //If we got CourseId & courseTitle then fetch RecordID
+                        //If we got CourseId then fetch RecordID
                         if (!string.IsNullOrEmpty(courseId))
                         {  //TODO: Fetch RecordID
                            //var query = @"Select  Co.Id
@@ -224,13 +235,13 @@ namespace MMU.Functions.Helpers
 
                             if (recordId <= 0)
                             {
-                                var query = @"Select cot.id from ACCourseofferingTemplate cot
-                                            Where cot.courseid ='" + courseId + "'";
-                                
+                                var query = $"Select cot.id from ACCourseofferingTemplate cot Where cot.courseid ='{courseId}'";
+
                                 try
                                 {
-                                    recordId = 2;
-                                    //recordId = _dataService.Query<int>("u4clone", query).FirstOrDefault();
+                                    recordId = _dataService.Query<int>("u4clone", query).FirstOrDefault();
+                                    //recordId = 2;
+                                    payload.Id = recordId;
                                 }
                                 catch (Exception ex)
                                 {
@@ -250,59 +261,88 @@ namespace MMU.Functions.Helpers
                                 //    endDate = row.GetCell(j).DateCellValue.ToString();
                                 //}
 
-                                if (string.IsNullOrEmpty(successValue) && string.IsNullOrEmpty(errorValue)) 
-                                    //&& !string.IsNullOrEmpty(startDate) && !string.IsNullOrEmpty(endDate))
+                                if (string.IsNullOrEmpty(successValue) && string.IsNullOrEmpty(errorValue))
                                 {
+                                    if (reference.StartsWith(MinEnrolledColumn)) //MinEnrolled
+                                    {
+                                        payload.MinEnrolled = Convert.ToInt32(row.GetCell(j).StringCellValue);
+                                    }
+                                    if (reference.StartsWith(MaxEnrolledColumn)) //MaxEnrolled
+                                    {
+                                        payload.MaxEnrolled = Convert.ToInt32(row.GetCell(j).StringCellValue);
+                                    }
+                                    if (reference.StartsWith(PriceGroupIdColumn)) //PriceGroupId
+                                    {
+                                        //TODO:
+                                        //payload.PriceGroupId = Convert.ToInt32(row.GetCell(j).StringCellValue);
+                                        payload.PriceGroupId = Convert.ToInt32(priceGroupList.Where(x => x.BusinessMeaningName == row.GetCell(j).StringCellValue).Select(y => y.Id));
+                                    }
+                                    if (reference.StartsWith(CourseLevelIdColumn)) //CourseLevelId
+                                    {
+                                        //TODO:
+                                        //payload.CourseLevelId = Convert.ToInt32(row.GetCell(j).StringCellValue);
+                                        payload.CourseLevelId = Convert.ToInt32(courseLevelList.Where(x => x.BusinessMeaningName == row.GetCell(j).StringCellValue).Select(y => y.Id));
+                                    }
+                                    if (reference.StartsWith(EnrollmentModeIdColumn)) //EnrollmentModeId
+                                    {
+                                        //TODO:
+                                        //payload.EnrollmentModeId = Convert.ToInt32(row.GetCell(j).StringCellValue);
+                                        payload.EnrollmentModeId = Convert.ToInt32(enrollmentModelList.Where(x => x.BusinessMeaningName == row.GetCell(j).StringCellValue).Select(y => y.Id));
+                                    }
+
                                     //bool validDates = ValidateDates(startDate, endDate);
 
                                     //if (validDates)
+
+                                    //Call Api to Update Start & End Dates
+                                    try
                                     {
-                                        //Call Api to Update Start & End Dates
-                                        try
+                                        //TODO
+                                        //var apiUri = new Uri("https://u4sm-preview-mmu.unit4cloud.com/U4SMapi/api/CourseOfferingTemplate/put?id=1");
+
+                                        var apiUri = new Uri("https://u4sm-preview-mmu.unit4cloud.com/U4SMapi/api/CourseOfferingTemplate/put?id=" + recordId);
+                                        //https://u4sm-accept05-mmu-sit.unit4cloud.com/U4SMapi/api/CourseOffering/get?id=1
+
+                                        //var payload = new CourseOfferingTemplate
+                                        //{
+                                        //    Id = recordId,
+                                        //    MinEnrolled = 1,
+                                        //    MaxEnrolled = 1,
+                                        //    PriceGroupId = 1,
+                                        //    CourseLevelId = 1,
+                                        //    EnrollmentModeId = 1
+                                        //    //StartDate = Convert.ToDateTime(startDate),
+                                        //    //EndDate = Convert.ToDateTime(endDate)
+                                        //};
+
+                                        var payloadString = JsonConvert.SerializeObject(payload);
+                                        //TODO: Uncomment for live
+                                        var message = await _messageFactory.CreateMessage(HttpMethod.Put, apiUri, payloadString);
+                                        httpResponseMessage = await _httpClient.SendAsync(message);
+                                        if (httpResponseMessage.StatusCode.Equals(System.Net.HttpStatusCode.OK))
+                                        //if (1 == 1)
                                         {
-                                            //TODO
-                                            var apiUri = new Uri("https://u4sm-preview-mmu.unit4cloud.com/U4SMapi/api/CourseOfferingTemplate/put?id=1");
-
-                                            var payload = new CourseOfferingTemplate
-                                            {
-                                                Id = recordId,
-                                                MinEnrolled = 
-                                                MaxEnrolled = 
-                                                PriceGroupId = 
-                                                CourseLevelId = 
-                                                EnrollmentModeId = 
-                                                //StartDate = Convert.ToDateTime(startDate),
-                                                //EndDate = Convert.ToDateTime(endDate)
-                                            };
-
-                                            var payloadString = JsonConvert.SerializeObject(payload);
-                                            //TODO: Uncomment for live
-                                            var message = await _messageFactory.CreateMessage(HttpMethod.Put, apiUri, payloadString);
-                                            httpResponseMessage = await _httpClient.SendAsync(message);
-                                            if (httpResponseMessage.StatusCode.Equals(System.Net.HttpStatusCode.OK))
-                                            //if (1 == 1)
-                                            {
-                                                //TODO: Update success
-                                                successValue = "Success";
-                                            }
-                                            else //failure
-                                            {
-                                                errorValue = httpResponseMessage.Content.ReadAsStringAsync();
-                                            }
+                                            //TODO: Update success
+                                            successValue = "Success";
                                         }
-                                        catch (Exception ex)
+                                        else //failure
                                         {
-                                            errorValue = ex;
+                                            errorValue = httpResponseMessage.Content.ReadAsStringAsync();
                                         }
                                     }
-                                    else
+                                    catch (Exception ex)
                                     {
-                                        invalidDates = "Invalid Start / End Dates";
-                                        successValue = "Fail";
+                                        errorValue = ex;
                                     }
+
+                                    //else
+                                    //{
+                                    //    invalidDates = "Invalid Start / End Dates";
+                                    //    successValue = "Fail";
+                                    //}
                                 }
 
-                                if (reference.StartsWith("K")) // RecordId
+                                if (reference.StartsWith(RecordIdColumn)) // RecordId
                                 {
                                     using FileStream wstr = new FileStream(fileName, FileMode.Create, FileAccess.Write);
                                     cell = row.GetCell(j);
@@ -311,7 +351,7 @@ namespace MMU.Functions.Helpers
                                     wstr.Close();
                                 }
 
-                                if (reference.StartsWith("L")) // Result : Success Or Failure
+                                if (reference.StartsWith(ResultColumn)) // Result : Success Or Failure
                                 {
                                     using FileStream wstr = new FileStream(fileName, FileMode.Create, FileAccess.Write);
                                     cell = row.GetCell(j);
@@ -320,7 +360,7 @@ namespace MMU.Functions.Helpers
                                     wstr.Close();
                                 }
 
-                                if (reference.StartsWith("M") && errorValue != null) // Error Reason
+                                if (reference.StartsWith(ErrorColumn) && errorValue != null) // Error Reason
                                 {
                                     using FileStream wstr = new FileStream(fileName, FileMode.Create, FileAccess.Write);
                                     cell = row.GetCell(j);
@@ -332,7 +372,7 @@ namespace MMU.Functions.Helpers
                             else
                             {
                                 //Log error to Excel and continue
-                                if (reference.StartsWith("M") && (recordId == 0)) // Error Reason
+                                if (reference.StartsWith(ErrorColumn) && (recordId == 0)) // Error Reason
                                 {
                                     using FileStream noIDStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
                                     cell = row.GetCell(j);
---
+++ MMU.Functions/Models/BusinessNames.cs
@@ -0,0 +1,8 @@
+﻿namespace MMU.Functions.Models
+{
+    public class BusinessNames
+    {
+        public int Id { get; set; }
+        public string BusinessMeaningName { get; set; }
+    }
+}
